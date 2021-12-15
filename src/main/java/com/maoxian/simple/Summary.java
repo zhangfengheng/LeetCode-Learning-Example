051d5e2f221746b882ca8ae234c2238231d356a4
zhangfengheng <zhangfengheng@consen.net>
2021-12-15 09:30:31 +0800

    第一次提交：两数之和

--- src/main/java/com/maoxian/simple/Summary.java
+++ src/main/java/com/maoxian/simple/Summary.java
@@ -12,6 +12,13 @@ import java.util.HashMap;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class Summary {
+
+  public static void main(String[] args) {
+    Summary summary = new Summary();
+    int[] a = {2,7,12,13,14};
+    summary.twoSum(a,9);
+  }
+
   /*
   * 题目：两数之和
   * 题目描述：给定一个整数数组 nums和一个整数目标值 target，请你在该数组中找出 和为目标值 target 的那两个整数，并返回它们的数组下标。
